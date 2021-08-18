c818b8afa3f0af03974ac0324f2c09b26dd83d5f
takurinton <taryochanman@icloud.com>
2021-08-18 03:25:08 +0000

    fix: token

--- controller/slack.go
+++ controller/slack.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"os"
 
 	"portfolio/model"
 	"portfolio/service"
@@ -23,28 +24,30 @@ func Hoge(c *gin.Context) {
 }
 
 func setToken() *slack.Client {
-	token := "xoxb-1978524669680-2332177912432-oniubBCI7jm3Fvfe8uligKmG"
+	token := os.Getenv("SLACK_TOKEN")
 	client := slack.New(token)
 	return client
 }
 
 // テスト用のエンドポイント
 func HelloWorld(c *gin.Context) {
-	client := setToken()
-
-	_, _, err := client.PostMessage("condition", slack.MsgOptionText("<@U01UFA1TP8R>", false))
-	if err != nil {
-		panic(err)
-	}
+	var test interface{}
+         if err := c.BindJSON(&test); err != nil {
+                c.Status(http.StatusBadRequest)
+                return
+        }
+        c.JSONP(http.StatusOK, test)
+        return
 }
 
 func Condition(c *gin.Context) {
 	// var test interface{}
 	// if err := c.BindJSON(&test); err != nil {
-	// 	c.Status(http.StatusBadRequest)
-	// 	return
+	// 	 c.Status(http.StatusBadRequest)
+	// 	 return
 	// }
 	// c.JSONP(http.StatusOK, test)
+	// return 
 
 	var json model.RequestConditionEventsType
 
