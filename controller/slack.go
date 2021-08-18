d58efff28d0a150412f90ad7399ed53a775e7f9a
takurinton <takurinton@takurinton.com>
2021-08-18 23:51:29 +0900

    setup

--- controller/slack.go
+++ controller/slack.go
@@ -1,15 +1,7 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
-	"regexp"
-	"strconv"
-	"strings"
-	"time"
-
-	"portfolio/model"
-	"portfolio/service"
 
 	"github.com/gin-gonic/gin"
 	"github.com/slack-go/slack"
@@ -39,52 +31,11 @@ func HelloWorld(c *gin.Context) {
 }
 
 func Condition(c *gin.Context) {
-	// var test interface{}
-	// if err := c.BindJSON(&test); err != nil {
-	// 	c.Status(http.StatusBadRequest)
-	// 	return
-	// }
-	// c.JSONP(http.StatusOK, test)
-
-	var json model.RequestConditionEventsType
-
-	if err := c.BindJSON(&json); err != nil {
+	var test interface{}
+	if err := c.BindJSON(&test); err != nil {
 		c.Status(http.StatusBadRequest)
 		return
 	}
-
-	if json.Event.Channel != "C023DBKUGHK" {
-		c.Status(http.StatusInternalServerError)
-		return
-	}
-
-	time.Local = time.FixedZone("Asia/Tokyo", 9*60*60)
-	time.LoadLocation("Asia/Tokyo")
-	datetime := time.Unix(int64(json.EventTime), 0)
-	user := json.Event.User
-	text := json.Event.Text
-
-	checkMental := regexp.MustCompile(`精神\d+(\.\d+)?`).Match([]byte(text))
-	checkPhysical := regexp.MustCompile(`体調\d+(\.\d+)?`).Match([]byte(text))
-	if !checkMental || !checkPhysical {
-		c.Status(http.StatusInternalServerError)
-		return
-	}
-	// (精神or体調)+(整数or小数) にマッチする正規表現
-	// 精神\d+(\.\d+)?
-	// 体調\d+(\.\d+)?
-	m := regexp.MustCompile(`精神\d+(\.\d+)?`)
-	p := regexp.MustCompile(`体調\d+(\.\d+)?`)
-	men := p.ReplaceAllString(text, "")
-	phy := m.ReplaceAllString(text, "")
-	mr := regexp.MustCompile(`精神`)
-	pr := regexp.MustCompile(`体調`)
-	_mental := mr.ReplaceAllString(men, "")
-	_physical := pr.ReplaceAllString(phy, "")
-	mental, _ := strconv.ParseFloat(strings.Replace(_mental, "\n", "", -1), 64)
-	physical, _ := strconv.ParseFloat(strings.Replace(_physical, "\n", "", -1), 64)
-	_, err := service.CreateCondition(user, mental, physical, datetime)
-	if err != nil {
-		fmt.Printf("error %v", err)
-	}
+	c.JSONP(http.StatusOK, test)
+	return
 }
